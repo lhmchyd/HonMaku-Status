b370b26e9d6e06860a4a1529c5da58f4d9a6f80b
lhmchyd <75568532+lhmchyd@users.noreply.github.com>
2025-10-28 01:08:40 +0700

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -6,10 +6,16 @@ function formatDateTime(date) {
     const year = d.getFullYear();
     const month = String(d.getMonth() + 1).padStart(2, '0');
     const day = String(d.getDate()).padStart(2, '0');
-    const hours = String(d.getHours()).padStart(2, '0');
+    
+    let hours = d.getHours();
     const minutes = String(d.getMinutes()).padStart(2, '0');
     const seconds = String(d.getSeconds()).padStart(2, '0');
-    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds} ${ampm}`;
 }
 
 function formatDateShort(date) {
